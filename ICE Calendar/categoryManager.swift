ba9afc5c97c0db6d27df1f04488143e81b85fc2c
asowers1 <asow123@gmail.com>
2015-04-23 15:09:24 -0400

    fixed the tableViewCell detailText in "All"

---
+++ ICE Calendar/categoryManager.swift
@@ -0,0 +1,13 @@
+//
+//  categoryManager.swift
+//  ICE Calendar
+//
+//  Created by Andrew Sowers on 4/23/15.
+//  Copyright (c) 2015 Andrew Sowers. All rights reserved.
+//
+
+import UIKit
+
+class categoryManager: NSObject {
+   
+}
