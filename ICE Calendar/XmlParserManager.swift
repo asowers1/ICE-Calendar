935553f0fead6494e90bce9d34a7374258151326
asowers1 <asow123@gmail.com>
2015-04-26 23:08:50 -0400

    added commenting for XmlParserManager

--- ICE Calendar/XmlParserManager.swift
+++ ICE Calendar/XmlParserManager.swift
@@ -3,12 +3,15 @@
 //  RSSwift
 //
 //  Created by Arled Kola on 20/10/2014.
+//  Modified by Andrew Sowers on 4/26/2015.
 //  Copyright (c) 2014 Arled. All rights reserved.
-//
+
 
 import Foundation
 
 class XmlParserManager: NSObject, NSXMLParserDelegate {
+    
+    // MARK: - instance methods
     
     var parser = NSXMLParser()
     var feeds = NSMutableArray()
@@ -19,12 +22,32 @@ class XmlParserManager: NSObject, NSXMLParserDelegate {
     var fdescription = NSMutableString()
     var fdate = NSMutableString()
     
-    // initilise parser
+    
+    // MARK: - instance methods
+    
+    /********************************************************************
+    *Function: startParse
+    *Purpose: initialize the parser
+    *Parameters: url:NSURL
+    *Return: AnyObject
+    *Properties NA
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
     func initWithURL(url :NSURL) -> AnyObject {
         startParse(url)
         return self
     }
     
+    /********************************************************************
+    *Function: startParse
+    *Purpose: start the parsing
+    *Parameters: url:NSURL
+    *Return: Void
+    *Properties NA
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
     func startParse(url :NSURL) {
         feeds = []
         parser = NSXMLParser(contentsOfURL: url)!
@@ -35,10 +58,29 @@ class XmlParserManager: NSObject, NSXMLParserDelegate {
         parser.parse()
     }
     
+    /********************************************************************
+    *Function: parser
+    *Purpose: return data feed
+    *Parameters: Void.
+    *Return: NSArray
+    *Properties NA
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
+
     func allFeeds() -> NSMutableArray {
         return feeds
     }
     
+    /********************************************************************
+    *Function: parser
+    *Purpose: parse XML
+    *Parameters: parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]
+    *Return: Void.
+    *Properties ftitle, link, fdescription, fdate
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
     func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [NSObject : AnyObject]) {
         element = elementName
         
@@ -57,7 +99,15 @@ class XmlParserManager: NSObject, NSXMLParserDelegate {
 
     }
     
-    
+    /********************************************************************
+    *Function: parser
+    *Purpose: parse XML
+    *Parameters: parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?
+    *Return: Void.
+    *Properties ftitle, link, fdescription, fdate
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
     func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         
         if (elementName as NSString).isEqualToString("item") {
@@ -82,6 +132,15 @@ class XmlParserManager: NSObject, NSXMLParserDelegate {
         
     }
     
+    /********************************************************************
+    *Function: parser
+    *Purpose: parse XML
+    *Parameters: parser: NSXMLParser, foundCharacters string: String?
+    *Return: Void.
+    *Properties ftitle, link, fdescription, fdate
+    *Precondition: NA
+    *Written by: Arled Kola
+    ********************************************************************/
     func parser(parser: NSXMLParser, foundCharacters string: String?) {
         
         if element.isEqualToString("title") {
